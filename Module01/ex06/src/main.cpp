c59d5845225a40f3a4296c0719e68cac708a3e52
umeneses <umeneses@student.42sp.org.br>
2025-07-10 13:01:48 -0300

    refactor error message

--- Module01/ex06/src/main.cpp
+++ Module01/ex06/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 18:29:21 by umeneses          #+#    #+#             */
-/*   Updated: 2025/07/08 19:35:25 by umeneses         ###   ########.fr       */
+/*   Updated: 2025/07/10 13:00:35 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,10 @@ int main(int argc, char **argv)
     if (argc != 2)
     {
         std::cerr
-        << "Usage: " << argv[0] << " <level>"
+        << "Error. Usage: " << argv[0] << " <level>"
         << std::endl;
         return 1;
     }
-
     Harl harl;
     harl.complain(argv[1]);
     return 0;
