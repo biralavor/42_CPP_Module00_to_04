3c879816f2dc25ea259a7fa0c24aba0689d04759
umeneses <umeneses@student.42sp.org.br>
2025-07-10 18:09:51 -0300

    #38 refactor module01 ex04 using classes

--- Module01/ex04/src/main.cpp
+++ Module01/ex04/src/main.cpp
@@ -6,23 +6,25 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 16:10:17 by umeneses          #+#    #+#             */
-/*   Updated: 2025/07/07 19:47:16 by umeneses         ###   ########.fr       */
+/*   Updated: 2025/07/10 17:52:22 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "DatabaseManager.hpp"
+#include "ReplaceThisWithThat.hpp"
 
 int main(int argc, char **argv)
 {
+    ReplaceThisWithThat replaceThis;
+
     if (argc != 4 || argv[1][0] == '\0'
         || argv[2][0] == '\0'  || argv[3][0] == '\0')
     {
         std::cerr
-        << "Usage: " << argv[0]
+        << "Error. Usage: " << argv[0]
         << " filename string_to_be_replaced string_to_replace_with"
         << std::endl;
         return 1;
     }
-    databaseManager(argv);
+    replaceThis.replaceManager(argv);
     return 0;
 }
