46a98058db49e1084c05ea466d9543ae40c7e60a
umeneses <umeneses@student.42sp.org.br>
2025-07-10 18:53:31 -0300

    refactor feedback error message

--- Module01/ex04/src/main.cpp
+++ Module01/ex04/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 16:10:17 by umeneses          #+#    #+#             */
-/*   Updated: 2025/07/10 18:14:43 by umeneses         ###   ########.fr       */
+/*   Updated: 2025/07/10 18:51:28 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int main(int argc, char **argv)
         || argv[2][0] == '\0'  || argv[3][0] == '\0')
     {
         std::cerr
-        << "Error. Invalid Arguments."
+        << "Error. Invalid Arguments.\n"
         << "Usage: " << argv[0]
         << " filename string_to_be_replaced string_to_replace_with"
         << std::endl;
