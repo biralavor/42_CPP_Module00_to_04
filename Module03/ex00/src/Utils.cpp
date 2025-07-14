c4bb2d8ccc194c72de111d00deba9dbe2f9687c9
Bira Lavor <umeneses@student.42sp.org.br>
2025-07-14 20:06:25 -0300

    #55 refactor module03 ex00

---
+++ Module03/ex00/src/Utils.cpp
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Utils.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/14 17:25:15 by umeneses          #+#    #+#             */
+/*   Updated: 2025/07/14 19:00:05 by umeneses         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Utils.hpp"
+
+void Utils::constructorTitlePrinter(std::string type, std::string objectName) const
+{
+    std::cout
+    << ":: " << type << " created: [" << YELLOW << objectName << RESET << "]"
+    << std::endl;
+}
+
+void Utils::destructorTitlePrinter(std::string type, std::string objectName) const
+{
+    std::cout
+	<< RED << "ᛝᛝᛝ " << type << " " << YELLOW << objectName
+	<< RED <<" destroyed. ᛝᛝᛝ" << RESET
+    << std::endl;
+}
+
+void Utils::statusConstructorPrinter(std::string type, int value) const
+{
+	std::cout
+	<< "| " << std::setw(13) << type << " :::: " << "[" << value << "]\n"
+	<< "---------------------------"
+	<< std::endl;
+}
+
+void Utils::statusEmojizerPrinter(std::string type, int value) const
+{
+	std::string emoji;
+	std::string emptySlot = "⬛";
+	int idx = 0;
+	
+	if (type == "hit")
+		emoji = "❤️ ";
+	else if (type == "energy")
+		emoji = "⚡️";
+	else if (type == "attack")
+		emoji = "💥";
+	else
+		emoji = "❓";
+	std::cout
+	<< "| " << std::setw(7) << type << " :::: " << "[";
+	while(idx++ < value)
+		std::cout << emoji;
+	while(idx++ <= 10)
+		std::cout << emptySlot;
+	std::cout << "]"
+	<< std::endl;
+}
+
+void Utils::roundTitlePrinter(std::string title) const
+{
+    int width = 30;
+    char paddingChar = ':';
+    int paddingSize = (width - title.length()) + 2;
+
+    std::cout << BLUE << std::string(width * 2, paddingChar) << std::endl;
+    std::cout << " " << std::string(paddingSize, paddingChar)
+	<< " " + title + " " << std::string(paddingSize, paddingChar) << std::endl;
+    std::cout << std::string(width * 2, paddingChar)
+	<< RESET << std::endl;
+}
+
+void Utils::zeroPointsPrinter(std::string classType, std::string pointsType,
+    std::string objectName) const
+{
+    std::cout << RED << classType << " " << YELLOW << objectName;
+    if (pointsType == "hit")
+    {
+        std::cout
+        << " is already "
+		<< RED << "dead!\n" << RESET
+		<< std::endl;
+    }
+    else if (pointsType == "energy")
+    {
+        std::cout
+		<< " has no energy left to repair! In other words: it is "
+		<< RED << "dead!\n" << RESET
+		<< std::endl;
+    }
+}
--- Module03/ex00/src/main.cpp
+++ Module03/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/13 13:12:58 by umeneses          #+#    #+#             */
-/*   Updated: 2025/07/13 18:05:32 by umeneses         ###   ########.fr       */
+/*   Updated: 2025/07/14 20:01:56 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,26 @@ int main(void)
 {
 	ClapTrap peanut("Peanut Butter");
 	ClapTrap sandwich("Sandwich");
-	
+
+	peanut._printer.roundTitlePrinter("ROUND 01");
 	peanut.attack("Sandwich");
 	peanut.takeDamage(3);
-	peanut.beRepaired(3);
-
+	peanut.beRepaired(2);
+	peanut.takeDamage(10);
+	peanut.takeDamage(3);
+	
+	sandwich._printer.roundTitlePrinter("ROUND 02");
 	sandwich.attack("Peanut Butter");
 	sandwich.takeDamage(8);
 	sandwich.beRepaired(5);
+	sandwich.attack("Peanut Butter");
+	sandwich.attack("Peanut Butter");
+	sandwich.attack("Peanut Butter");
+	sandwich.attack("Peanut Butter");
+	sandwich.attack("Peanut Butter");
+	sandwich.attack("Peanut Butter");
+	sandwich.beRepaired(1);
+	sandwich.beRepaired(1);
 
 	return 0;
 }
