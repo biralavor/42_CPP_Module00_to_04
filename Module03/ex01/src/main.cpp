f9595f93760051f1e27c924a2b00618d03a99353
Bira Lavor <umeneses@student.42sp.org.br>
2025-07-14 18:07:40 -0300

    feat more tests to archieve dead mode

--- Module03/ex01/src/main.cpp
+++ Module03/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/13 13:12:58 by umeneses          #+#    #+#             */
-/*   Updated: 2025/07/14 17:51:56 by umeneses         ###   ########.fr       */
+/*   Updated: 2025/07/14 18:01:34 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,22 @@ int main(void)
 	printer.roundTitlePrinter("ROUND 01");
 	peanut.attack("Sandwich");
 	peanut.takeDamage(3);
-	peanut.beRepaired(3);
+	peanut.beRepaired(2);
+	peanut.takeDamage(10);
+	peanut.takeDamage(3);
 
 	printer.roundTitlePrinter("ROUND 02");
 	sandwich.attack("Peanut Butter");
 	sandwich.takeDamage(8);
 	sandwich.beRepaired(5);
+	sandwich.attack("Peanut Butter");
+	sandwich.attack("Peanut Butter");
+	sandwich.attack("Peanut Butter");
+	sandwich.attack("Peanut Butter");
+	sandwich.attack("Peanut Butter");
+	sandwich.attack("Peanut Butter");
+	sandwich.beRepaired(1);
+	sandwich.beRepaired(1);
 
 	printer.roundTitlePrinter("ROUND 03");
 	scavTrap.attack("Peanut Butter");
