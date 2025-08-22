00e254e176ba5ae6bb735948f091ed5169cef24e
umeneses <umeneses@student.42sp.org.br>
2025-08-22 12:56:15 -0300

    fix gradeTooHigh and GradeTooLow

--- Module05/ex00/headers/Bureaucrat.hpp
+++ Module05/ex00/headers/Bureaucrat.hpp
@@ -6,32 +6,47 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/20 15:06:59 by umeneses          #+#    #+#             */
-/*   Updated: 2025/08/20 17:32:51 by umeneses         ###   ########.fr       */
+/*   Updated: 2025/08/22 12:48:15 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include <iostream>
 #include <string>
-#include "Utils.hpp"
-
+#include <exception>
 class Bureaucrat
 {
     const std::string   _name;
     int                 _grade;
 
 public:
+    static const int    HIGHEST_GRADE = 1;
+    static const int    LOWEST_GRADE = 150;
+
+public:
     Bureaucrat(void);
     Bureaucrat(std::string name, int grade);
     ~Bureaucrat(void);
     Bureaucrat(Bureaucrat const &src);
-    
+    Bureaucrat &operator=(Bureaucrat const &rightSide);
+
     std::string getName(void) const;
     int getGrade(void) const;
-    void setGrade(int grade);
+
     void incrementGrade(void);
     void decrementGrade(void);
 
-public:
-    Utils _printer;
+    class GradeTooHighException : public std::exception
+    {
+    public:
+        virtual const char* what() const throw();
+    };
+
+    class GradeTooLowException : public std::exception
+    {
+    public:
+        virtual const char* what() const throw();
+    };
 };
+
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &bureaucrat);
--- Module05/ex00/src/Bureaucrat.cpp
+++ Module05/ex00/src/Bureaucrat.cpp
@@ -6,22 +6,41 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/20 14:59:45 by umeneses          #+#    #+#             */
+<<<<<<< Updated upstream
 /*   Updated: 2025/08/20 18:00:23 by umeneses         ###   ########.fr       */
+=======
+/*   Updated: 2025/08/22 12:39:07 by umeneses         ###   ########.fr       */
+>>>>>>> Stashed changes
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
 Bureaucrat::Bureaucrat(void)
+<<<<<<< Updated upstream
     : _name("Default"), _grade(150)
+=======
+    : _name("Default"), _grade(LOWEST_GRADE)
+>>>>>>> Stashed changes
 {
     std::cout << "Default constructor called" << std::endl;
 }
 
+<<<<<<< Updated upstream
 Bureaucrat::Bureaucrat(std::string name, int grade)
     : _name(name), _grade(grade)
 {
     std::cout << "Parameterized constructor called" << std::endl;
+=======
+Bureaucrat::Bureaucrat(const std::string name, int grade)
+    : _name(name), _grade(grade)
+{
+    std::cout << "Parameterized constructor called" << std::endl;
+    if (grade < HIGHEST_GRADE)
+        throw Bureaucrat::GradeTooHighException();
+    if (grade > LOWEST_GRADE)
+        throw Bureaucrat::GradeTooLowException();
+>>>>>>> Stashed changes
 }
 
 Bureaucrat::~Bureaucrat(void)
@@ -30,14 +49,27 @@ Bureaucrat::~Bureaucrat(void)
 }
 
 Bureaucrat::Bureaucrat(Bureaucrat const &src)
+    : _name(src._name)
 {
-    std::cout
-    << "Copy constructor called for: "
-    << YELLOW << _name << RESET
-    << std::endl;
+    std::cout << "Copy constructor called for: " << _name << std::endl;
     *this = src;
 }
 
+<<<<<<< Updated upstream
+=======
+Bureaucrat &Bureaucrat::operator=(Bureaucrat const &rightSide)
+{
+    std::cout << "Assignment operator called for: " << rightSide.getName() << std::endl;
+    if (this != &rightSide)
+    {
+        // _name is const, so it cannot be changed after construction.
+        // Only non-const members can be assigned.
+        this->_grade = rightSide.getGrade();
+    }
+    return *this;
+}
+
+>>>>>>> Stashed changes
 std::string Bureaucrat::getName(void) const
 {
     return this->_name;
@@ -50,14 +82,19 @@ int Bureaucrat::getGrade(void) const
 
 void Bureaucrat::incrementGrade(void)
 {
+    if (this->_grade - 1 < HIGHEST_GRADE)
+        throw Bureaucrat::GradeTooHighException();
     this->_grade--;
 }
 
 void Bureaucrat::decrementGrade(void)
 {
+    if (this->_grade + 1 > LOWEST_GRADE)
+        throw Bureaucrat::GradeTooLowException();
     this->_grade++;
 }
 
+<<<<<<< Updated upstream
 void Bureaucrat::setGrade(int grade)
 {
     if (grade < 1)
@@ -66,4 +103,20 @@ void Bureaucrat::setGrade(int grade)
         throw Bureaucrat::GradeTooLowException();
     else
         this->_grade = grade;
+=======
+const char* Bureaucrat::GradeTooHighException::what() const throw()
+{
+    return "Grade is too high";
+}
+
+const char* Bureaucrat::GradeTooLowException::what() const throw()
+{
+    return "Grade is too low";
+}
+
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &bureaucrat)
+{
+    out << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade();
+    return out;
+>>>>>>> Stashed changes
 }
