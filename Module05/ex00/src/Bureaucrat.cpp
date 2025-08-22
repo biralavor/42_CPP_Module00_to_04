3368e842ba014c757686fb4402925b2dab430df6
umeneses <umeneses@student.42sp.org.br>
2025-08-22 12:57:03 -0300

    fix Bureaucrat

--- Module05/ex00/src/Bureaucrat.cpp
+++ Module05/ex00/src/Bureaucrat.cpp
@@ -6,32 +6,18 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/20 14:59:45 by umeneses          #+#    #+#             */
-<<<<<<< Updated upstream
-/*   Updated: 2025/08/20 18:00:23 by umeneses         ###   ########.fr       */
-=======
-/*   Updated: 2025/08/22 12:39:07 by umeneses         ###   ########.fr       */
->>>>>>> Stashed changes
+/*   Updated: 2025/08/22 12:55:45 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
 Bureaucrat::Bureaucrat(void)
-<<<<<<< Updated upstream
-    : _name("Default"), _grade(150)
-=======
     : _name("Default"), _grade(LOWEST_GRADE)
->>>>>>> Stashed changes
 {
     std::cout << "Default constructor called" << std::endl;
 }
 
-<<<<<<< Updated upstream
-Bureaucrat::Bureaucrat(std::string name, int grade)
-    : _name(name), _grade(grade)
-{
-    std::cout << "Parameterized constructor called" << std::endl;
-=======
 Bureaucrat::Bureaucrat(const std::string name, int grade)
     : _name(name), _grade(grade)
 {
@@ -40,7 +26,6 @@ Bureaucrat::Bureaucrat(const std::string name, int grade)
         throw Bureaucrat::GradeTooHighException();
     if (grade > LOWEST_GRADE)
         throw Bureaucrat::GradeTooLowException();
->>>>>>> Stashed changes
 }
 
 Bureaucrat::~Bureaucrat(void)
@@ -55,8 +40,6 @@ Bureaucrat::Bureaucrat(Bureaucrat const &src)
     *this = src;
 }
 
-<<<<<<< Updated upstream
-=======
 Bureaucrat &Bureaucrat::operator=(Bureaucrat const &rightSide)
 {
     std::cout << "Assignment operator called for: " << rightSide.getName() << std::endl;
@@ -69,7 +52,6 @@ Bureaucrat &Bureaucrat::operator=(Bureaucrat const &rightSide)
     return *this;
 }
 
->>>>>>> Stashed changes
 std::string Bureaucrat::getName(void) const
 {
     return this->_name;
@@ -94,16 +76,6 @@ void Bureaucrat::decrementGrade(void)
     this->_grade++;
 }
 
-<<<<<<< Updated upstream
-void Bureaucrat::setGrade(int grade)
-{
-    if (grade < 1)
-        throw Bureaucrat::GradeTooHighException();
-    else if (grade > 150)
-        throw Bureaucrat::GradeTooLowException();
-    else
-        this->_grade = grade;
-=======
 const char* Bureaucrat::GradeTooHighException::what() const throw()
 {
     return "Grade is too high";
@@ -118,5 +90,4 @@ std::ostream &operator<<(std::ostream &out, const Bureaucrat &bureaucrat)
 {
     out << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade();
     return out;
->>>>>>> Stashed changes
 }
