174ca6189875fb3ab4d7479040df658f9d1579f3
umeneses <umeneses@student.42sp.org.br>
2025-08-27 12:27:44 -0300

    feat: all methods for Shrubbery request form

--- Module05/ex02/src/ShrubberyCreationForm.cpp
+++ Module05/ex02/src/ShrubberyCreationForm.cpp
@@ -6,20 +6,69 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/25 14:10:25 by umeneses          #+#    #+#             */
-/*   Updated: 2025/08/25 14:10:51 by umeneses         ###   ########.fr       */
+/*   Updated: 2025/08/27 12:08:20 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ShrubberyCreationForm.hpp"
 
-ShrubberyCreationForm::ShrubberyCreationForm()
+ShrubberyCreationForm::ShrubberyCreationForm(void)
+	: AForm("Default Shrubbery Creation", _minGradeToSign, _minGradeToExecute)
 {
-	// constructor implementation
 }
 
-
+ShrubberyCreationForm::ShrubberyCreationForm(const std::string &target)
+	: AForm(target, _minGradeToSign, _minGradeToExecute)
+{
+}
 
 ShrubberyCreationForm::~ShrubberyCreationForm()
 {
-	// destructor implementation
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(ShrubberyCreationForm const &src)
+	: AForm(src)
+{
+}
+
+ShrubberyCreationForm &ShrubberyCreationForm::operator=(ShrubberyCreationForm const &rightSide)
+{
+	if (this != &rightSide)
+		AForm::operator=(rightSide);
+	return *this;
+}
+
+const char * ShrubberyCreationForm::FileOperationException::what() const throw()
+{
+	return "Error creating or writing to the shrubbery file.";
+}
+
+void ShrubberyCreationForm::writeShrubberyArt(std::ofstream &ofs) const
+{
+	ofs << "       _-_" << std::endl;
+	ofs << "    /~~   ~~\\" << std::endl;
+	ofs << " /~~         ~~\\" << std::endl;
+	ofs << "{               }" << std::endl;
+	ofs << " \\  _-     -_  /" << std::endl;
+	ofs << "   ~  \\\\ //  ~" << std::endl;
+	ofs << "_- -   | | _- _" << std::endl;
+	ofs << "  _ -  | |   -_" << std::endl;
+	ofs << "      // \\\\" << std::endl;
+}
+
+void ShrubberyCreationForm::createShrubberyFile(void) const
+{
+	std::string filename = this->getTarget() + "_shrubbery";;
+	std::ofstream ofs(filename.c_str());;
+
+	if (!ofs || ofs.fail() || !ofs.is_open())
+		throw ShrubberyCreationForm::FileOperationException();
+	else
+		writeShrubberyArt(ofs);
+}
+
+void ShrubberyCreationForm::action(Bureaucrat const &executor) const
+{
+	requirementsController(executor);
+	createShrubberyFile();
 }
